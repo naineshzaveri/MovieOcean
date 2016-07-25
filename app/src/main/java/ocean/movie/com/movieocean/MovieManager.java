7075502f4cc5f796e8b760431e54fd8a7393c0fe
naineshzaveri123 <naineshzaveri@gmail.com>
2016-07-25 10:33:18 +0530

    generic callback method for volley request

--- app/src/main/java/ocean/movie/com/movieocean/MovieManager.java
+++ app/src/main/java/ocean/movie/com/movieocean/MovieManager.java
@@ -5,9 +5,23 @@ import ocean.movie.com.movieocean.models.MovieModel;
 /**
  * Created by naineshzaveri on 24/07/16.
  */
-public class MovieManager {
+public class MovieManager implements com.android.volley.Response.Listener, com.android.volley.Response.ErrorListener {
     private static String TAG = MovieManager.class.getName();
     com.android.volley.Request.Priority priority = com.android.volley.Request.Priority.HIGH;
+    @Override
+    public void onErrorResponse(com.android.volley.VolleyError volleyError) {
+        if(movieValueListener != null){
+            movieValueListener.onReturnError(volleyError);
+        }
+    }
+
+    @Override
+    public void onResponse(Object o) {
+        com.google.gson.Gson gson = new com.google.gson.Gson();
+        if(movieValueListener != null){
+            movieValueListener.onReturnMovieList(gson.fromJson(o.toString(), ocean.movie.com.movieocean.models.MovieResponseModel.class).getResults());
+        }
+    }
 
     public interface MovieValueListener {
 
@@ -37,32 +51,7 @@ public class MovieManager {
         final String MOVIEW_URL = ocean.movie.com.movieocean.helper.WebHelper.RequestUrl.MOVIE_LIST + ocean.movie.com.movieocean.helper.WebHelper.Params.API_KEY + "=" + ocean.movie.com.movieocean.helper.WebHelper.API_KEY;
 
         com.android.volley.toolbox.StringRequest strReq = new com.android.volley.toolbox.StringRequest(com.android.volley.Request.Method.GET,
-                MOVIEW_URL, new com.android.volley.Response.Listener<String>() {
-
-            @Override
-            public void onResponse(String response) {
-                android.util.Log.d(TAG, response.toString());
-                 com.google.gson.Gson gson = new com.google.gson.Gson();
-                if(movieValueListener != null){
-                    movieValueListener.onReturnMovieList(gson.fromJson(response, ocean.movie.com.movieocean.models.MovieResponseModel.class).getResults());
-                }
-            }
-        }, new com.android.volley.Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(com.android.volley.VolleyError error) {
-                if(movieValueListener != null){
-                    movieValueListener.onReturnError(error);
-                }
-
-            }
-        }) {
-            @Override
-            public Priority getPriority() {
-                return priority;
-            }
-
-        };
+                MOVIEW_URL,this,this );
         // Adding request to request queue
         ocean.movie.com.movieocean.MovieOcean.getInstance().addToRequestQueue(strReq, TAG);
     }
@@ -74,32 +63,7 @@ public class MovieManager {
                 "&"+ ocean.movie.com.movieocean.helper.WebHelper.HeaderParams.SORT_BY + "="+ocean.movie.com.movieocean.helper.WebHelper.PARAMS_VALUE.SORT_BY_HIGEST_RATING;
 
         com.android.volley.toolbox.StringRequest strReq = new com.android.volley.toolbox.StringRequest(com.android.volley.Request.Method.GET,
-                MOVIEW_URL, new com.android.volley.Response.Listener<String>() {
-
-            @Override
-            public void onResponse(String response) {
-                android.util.Log.d(TAG, response.toString());
-                com.google.gson.Gson gson = new com.google.gson.Gson();
-                if(movieValueListener != null){
-                    movieValueListener.onReturnMovieList(gson.fromJson(response, ocean.movie.com.movieocean.models.MovieResponseModel.class).getResults());
-                }
-            }
-        }, new com.android.volley.Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(com.android.volley.VolleyError error) {
-                if(movieValueListener != null){
-                    movieValueListener.onReturnError(error);
-                }
-
-            }
-        }) {
-            @Override
-            public Priority getPriority() {
-                return priority;
-            }
-
-        };
+                MOVIEW_URL, this,this);
         // Adding request to request queue
         ocean.movie.com.movieocean.MovieOcean.getInstance().addToRequestQueue(strReq, TAG);
     }
@@ -141,7 +105,5 @@ public class MovieManager {
         // Adding request to request queue
         ocean.movie.com.movieocean.MovieOcean.getInstance().addToRequestQueue(strReq, TAG);
     }
-
-
 
 }
